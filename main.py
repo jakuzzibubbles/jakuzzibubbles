2792c98e37bf77508de2fed6661ee19c8b89425b
jakuzzibubbles <jakuzzi263@gmail.com>
2024-03-07 01:59:00 +0100

    ersterCode

---
+++ main.py
@@ -0,0 +1,14 @@
+name = input("Bitte gebe den Namen hier ein:")
+mein_name = "Linh"
+city = "Berlin"
+
+is_woman = True
+
+if is_woman:
+    print("Sehr geehrte Frau " + name + ",")
+
+else:
+    print("Lieber Herr " + name + ",")
+print(" ")
+print("Ich heiße " + mein_name + ".")
+print("Ich komme aus " + city + " und suche eine neue Wohnung.")
